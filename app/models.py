bd8b8ad6e1aef63bcbcd2ae20bc183426a5d5821
b3d0la9ich <tnikita912@gmail.com>
2025-09-23 22:48:48 +0700

    sadf

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash  # <-- добавь
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +11,13 @@ class User(db.Model, UserMixin):
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     submissions = db.relationship('Submission', backref='author', lazy=True)
+
+    # ===== работа с паролем =====
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.email}>"
